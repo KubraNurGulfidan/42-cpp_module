ba5bf806bf074c94b0f7d1f8014453a5cb542c02
Kübra Nur Gülfidan <kgulfida@k1m35s01.42istanbul.com.tr>
2025-02-10 19:51:04 +0300

    cpp00 ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgulfida <kgulfida@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 14:13:35 by kgulfida          #+#    #+#             */
+/*   Updated: 2025/02/10 14:37:53 by kgulfida         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+			std::cout << (char)toupper(av[i][j]);
+	}
+	std::cout << std::endl;
+}
